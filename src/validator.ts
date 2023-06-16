76daa8798dda4bd3cb01ac341778ade730628435
Jake Shilling <shilling.jake@gmail.com>
2023-06-16 09:29:29 -0400

    Create validator module

---
+++ src/validator.ts
@@ -0,0 +1,68 @@
+/**
+ * ```ts
+ * export interface Validator<A, B> {
+ *   (f: (a: A) => B): (a: A) => Validation<B>;
+ * }
+ * ```
+ *
+ * `Validator<A, B>` is a type that can take a function A => B and provide
+ * runtime validation to it.
+ *
+ * @example
+ * import * as t from 'io-ts';
+ * import { isLeft, isRight } from 'fp-ts/lib/Either';
+ * import { pipe } from 'fp-ts/lib/funciton';
+ *
+ * const UserCodec = t.type({
+ * name: t.string,
+ *  password: t.string,
+ * });
+ *
+ * type User = t.TypeOf<typeof UserCodec>;
+ *
+ * const ValidUser = { name: 'foo', password: 'bar' };
+ * const InvalidUser = { name: 'foo' };
+ *
+ * const f = (user: User) => user.password; // User => string
+ * const f2 = pipe(
+ *   f,
+ *   fromCodec(UserCodec),
+ * );  // unknown => t.Validation<string>
+ *
+ * assert.deepStrictEqual(isRight(f2(ValidUser)), true);
+ * assert.deepStrictEqual(isLeft(f2(InvalidUser)), true);
+ *
+ * @since 0.0.1
+ */
+
+import * as E from 'fp-ts/lib/Either';
+import { flow } from "fp-ts/lib/function";
+import { Decoder, Validation } from "io-ts";
+
+
+// -------------------------------------------------------------------------------------
+// model
+// -------------------------------------------------------------------------------------
+
+/**
+ * @category model
+ * @since 0.0.1
+ */
+export interface Validator<A, B> {
+    (f: (a: A) => B): (a: A) => Validation<B>;
+}
+
+// -------------------------------------------------------------------------------------
+// constructors
+// -------------------------------------------------------------------------------------
+
+/**
+ * @category constructors
+ * @since 0.0.1
+ */
+export function fromCodec<A, B>(codec: Decoder<unknown, A>): Validator<A, B> {
+    return (f) => flow(
+        codec.decode,
+        E.map(f)
+    );
+}
