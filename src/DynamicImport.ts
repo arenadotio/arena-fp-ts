1a4c2accfc6ad9e99b7f46d1d76e413eb2f36709
Jake Shilling <shilling.jake@gmail.com>
2023-06-21 08:09:47 -0400

    Add DynamicImport and Datadog

---
+++ src/DynamicImport.ts
@@ -0,0 +1,79 @@
+/**
+ * @since 0.0.1
+ */
+
+import * as O from 'fp-ts/lib/Option';
+import * as TO from 'fp-ts/lib/TaskOption';
+import * as S from 'fp-ts/lib/Semigroup';
+import * as M from 'fp-ts/lib/Monoid';
+import { pipe } from 'fp-ts/lib/function';
+
+// -------------------------------------------------------------------------------------
+// model
+// -------------------------------------------------------------------------------------
+
+/**
+ * @category model
+ * @since 0.0.1
+ */
+export type DynamicImport<T = any> = TO.TaskOption<T>;
+
+// -------------------------------------------------------------------------------------
+// constructors
+// -------------------------------------------------------------------------------------
+
+/**
+ * @category constructors
+ * @since 0.0.1
+ */
+
+export function fromModule<A = any>(name: string): TO.TaskOption<A> {
+    return async () => {
+        try {
+            return O.some(await import(name));
+        } catch {
+            return O.none;
+        }
+    }
+}
+
+// -------------------------------------------------------------------------------------
+// instances
+// -------------------------------------------------------------------------------------
+
+/**
+ * @internal
+ */
+const _concat = <A>(first: DynamicImport<A>, second: DynamicImport<A>): TO.TaskOption<A> => pipe(
+    first,
+    TO.alt(() => second),
+);
+
+/**
+ * @category instances
+ * @since 0.0.1
+ */
+export const Semigroup: S.Semigroup<DynamicImport> = {
+    concat: _concat,
+};
+
+/**
+ * @category instances
+ * @since 0.0.1
+ */
+export const Monoid: M.Monoid<DynamicImport> = {
+    concat: Semigroup.concat,
+    empty: TO.none,
+};
+
+// -------------------------------------------------------------------------------------
+// utils
+// -------------------------------------------------------------------------------------
+
+/**
+ * @category utils
+ * @since 0.0.1
+ */
+ export function firstSome(...mas: ReadonlyArray<DynamicImport>) {
+    return M.concatAll(Monoid)(mas);
+}
