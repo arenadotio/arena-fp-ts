a421e95b696461e5e77ff790c64493363de94f37
Jake Shilling <shilling.jake@gmail.com>
2023-06-21 12:42:12 -0400

    Package (#8)
    * Setup local publishing with `yarn publish`
    
    * Update github publish workflow
    
    * FIX secrets cannot start with GITHUB
    
    * FIX secrets cannot start with GITHUB

--- src/validator.ts
+++ src/validator.ts
@@ -12,7 +12,7 @@
  * import * as t from 'io-ts';
  * import { isLeft, isRight } from 'fp-ts/lib/Either';
  * import { pipe } from 'fp-ts/lib/function';
- * import { fromCodec } from 'arena-fp-ts/validator';
+ * import { fromCodec } from '@arenadotio/arena-fp-ts/validator';
  *
  * const UserCodec = t.type({
  * name: t.string,
