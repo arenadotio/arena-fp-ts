9acdccf318e77659274a4f83506d8dca0b742278
Jake Shilling <shilling.jake@gmail.com>
2023-07-03 14:26:22 -0400

    Remove package scope (#13)
    @arenadotio is taken on npm, so we need to drop it to be able to publish to the
    npm registry.

--- src/WrapperWithContext.ts
+++ src/WrapperWithContext.ts
@@ -5,7 +5,7 @@
  * @example
  * import { log } from 'fp-ts/lib/Console';
  * import { IO, Chain } from 'fp-ts/lib/IO';
- * import { wrapWithContextT } from '@arenadotio/arena-fp-ts/WrapperWithContext';
+ * import { wrapWithContextT } from 'arena-fp-ts/WrapperWithContext';
  *
  * const sqr = (x: number) => x * x;
  *
